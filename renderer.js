820de3a31d69d1ead2649aedc9b10fc42c6892e5
Pasikaas <hienlanhtute123@gmail.com>
2024-11-07 05:01:47 +0700

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -7,7 +7,7 @@ async function getRandomUrls(filePath, count) {
  }
  
  async function loadImages() {
-    const urlFilePath = 'file:\\\image_urls.txt';
+    const urlFilePath = '.\image_urls.txt';
     const urls = await getRandomUrls(urlFilePath, 500);
     const columns = ['col-1'];
     let columnIndex = 0;
